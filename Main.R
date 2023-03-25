74111c5a8c14684c68de060ba89c3dd621273d1b
ooleole0 <oo4766@gmail.com>
2023-03-25 09:47:19 +0800

    join SIC, create carbon-time-series-related variables, read stock data

--- Main.R
+++ Main.R
@@ -14,9 +14,12 @@ carbon <- read_csv("carbon.csv", na="") %>%
            scope3_up
     )
 
+SIC_sheet <- read_csv("SIC_sheet.csv", col_types = "c", na="")
+
 carb_fin <- read_csv("financial.csv", na="") %>%
     select(
         gvkey,
+        sic,
         datadate,
         fyear,
         act,
@@ -24,6 +27,8 @@ carb_fin <- read_csv("financial.csv", na="") %>%
         revt
     ) %>%
     left_join(carbon, by = c("gvkey", "fyear")) %>%
+    left_join(SIC_sheet, by = "sic") %>%
+    # create carbon-related variables
     mutate(
         scope1_2 = scope1 + scope2,
         scope1_2_3 = scope1 + scope2 + scope3_up,
@@ -33,4 +38,15 @@ carb_fin <- read_csv("financial.csv", na="") %>%
         inten_asset_1_2_3 = scope1_2_3 / at,
         inten_asset_long_1_2 = scope1_2 / (at - act),
         inten_asset_long_1_2_3 = scope1_2_3 / (at - act)
-    )
+    ) %>%
+    group_by(gvkey) %>%
+    mutate(
+        scope1_2_grow = scope1_2 - lag(scope1_2),
+        scope1_2_grow_r = scope1_2_grow / lag(scope1_2)
+    ) %>%
+    ungroup()
+
+# remove unnecessary datasheets
+remove(carbon, SIC_sheet)
+
+stock <- read_csv("stock.csv", na = "")
