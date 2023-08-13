b40188e923f40d8a91778174bde5447368995d43
ooleole0 <oo4766@gmail.com>
2023-08-13 17:34:35 +0800

    create adjustable rolling beta function

--- RollingBeta.R
+++ RollingBeta.R
@@ -20,19 +20,28 @@ port_us <- read_csv("PORT_USMARKET_LS_ff.csv") %>%
 tej_port_us <- tej %>%
     right_join(port_us, by = join_by(date == date))
 
+# create formula lists and names
+mode_tej_LS <- lapply(
+    paste(
+    "Ret", names(tej_port_us)[13:ncol(tej_port_us)], sep = "~"
+    ), formula
+)
+names(mode_tej_LS) <- names(tej_port_us)[13:ncol(tej_port_us)]
+
 # create a function which checks on if the time series is too short,
 # if not, return beta.
-estimate_capm <- function(data, min_obs = 1) {
+
+estimate_capm <- function(data, min_obs = 1, var_port) {
     if (nrow(data) < min_obs) {
         beta <- as.numeric(NA)
     } else {
-        fit <- lm(Ret ~ scope1_LS, data = data)
+        fit <- lm(formula = mode_tej_LS[[var_port]], data = data)
         beta <- as.numeric(coefficients(fit)[2])
     }
     return(beta)
 }
 
-roll_capm_estimation <- function(data, months, min_obs) {
+roll_capm_estimation <- function(data, var_port) {
     # arrange all rows
     data <- data %>%
         arrange(date)
@@ -41,8 +50,10 @@ roll_capm_estimation <- function(data, months, min_obs) {
         .x = data,
         .i = data$date,
         .period = "month",
-        .f = ~ estimate_capm(., min_obs),
-        .before = months - 1,
+        # minimum observation as 48
+        .f = ~ estimate_capm(., min_obs = 48, var_port),
+        # backtest period as 60(59+1)
+        .before = 59,
         .complete = FALSE
     )
     # return a tibble with months and corresponding beta estimates
@@ -52,29 +63,39 @@ roll_capm_estimation <- function(data, months, min_obs) {
     ))
 }
 
-# take 1101 as the example
-beta_example <- tej_port_us %>%
-    filter(Ticker == 1101) %>%
-    mutate(roll_capm_estimation(pick(everything()), months = 60, min_obs = 48)) %>%
-    drop_na() %>%
-    select(Ticker, CName, date, Ret, scope1_LS, beta) %>%
-    mutate_if(is.numeric, round, digits = 3)
-
-# betas group by Tickers
-tej_port_us_nested <- tej_port_us %>%
-    nest(data = c(date, Ret, scope1_LS), .by = c(Ticker, BCode, BName))
-
+# use multi-cores for speed
 plan(multisession, workers = availableCores())
 
-beta_nested <- tej_port_us_nested %>%
-    mutate(beta = furrr::future_map(
-        data,
-        ~ roll_capm_estimation(., months = 60, min_obs = 48)
-    )) %>%
-    unnest(beta) %>%
-    select(Ticker, date, beta_monthly = beta) %>%
-    drop_na() %>%
-    mutate_if(is.numeric, round, digits = 3)
+# estimate betas group by Tickers
+# pass data and the "name" of the portfolio you want to reg to in the function
+estimate_beta <- function(data, var_port){
+    # create nested data
+    data_nested <- data %>%
+        nest(data = c(date, Ret, {{var_port}}), .by = c(Ticker, BCode, BName))
+    
+    data_nested %>%
+        mutate(
+            beta = furrr::future_map(
+                data,
+                ~ roll_capm_estimation(., var_port)
+            )
+        ) %>%
+        unnest(beta) %>%
+        select(
+            Ticker, 
+            date,
+            beta
+        ) %>%
+        rename_with(
+            ~ paste({{var_port}}, "_beta", sep = ""),
+            beta
+        ) %>%
+        drop_na()
+}
+
+
+
+# beta distribution boxplot by industry
 
 tej_port_us %>%
     left_join(beta_nested, by = c("Ticker", "date")) %>%
@@ -90,7 +111,21 @@ tej_port_us %>%
         title = "Firm-specific beta distributions by industry"
     )
 
-# create all TW market beta
-tw_beta <- roll_capm_estimation(tej_port_us, 60, 36)
-
-alltw <- lm(beta ~ 1, data = tw_beta)
+beta_nested %>%
+    drop_na(beta_monthly) %>%
+    group_by(date) %>%
+    reframe(
+        x = quantile(beta_monthly, seq(0.1, 0.9, 0.1)),
+        quantile = 100 * seq(0.1, 0.9, 0.1)
+    ) %>%
+    ggplot(aes(
+        x = date, 
+        y = x, 
+        color = as_factor(quantile),
+        linetype = as_factor(quantile)
+    )) +
+    geom_line() +
+    labs(
+        x = NULL, y = NULL, color = NULL, linetype = NULL,
+        title = "Monthly deciles of estimated betas",
+    )
